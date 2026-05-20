a3c4c7da3d5bfb4c9ba9c2ff601e6d1172526527
Arjun Jayakumar <arjunjayakumar34@gmail.com>
2026-05-20 14:22:06 +0530

    Add GET endpoint for task status

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import create_task
+from .views import create_task , get_task
 
 urlpatterns = [
     path("tasks/", create_task, name="create_task"),
+    path("tasks/<int:task_id>/", get_task),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -5,6 +5,8 @@ from rest_framework import status
 from .models import Task
 from .serializers import TaskSerializer
 
+from django.shortcuts import get_object_or_404
+
 @api_view(["POST"])
 def create_task(request):
     serializer = TaskSerializer(data=request.data)
@@ -14,3 +16,11 @@ def create_task(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["GET"])
+def get_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+
+    serializer = TaskSerializer(task)
+
+    return Response(serializer.data)
