0868a8ef143ba5327314a781e9027a7a7da99c49
Arjun Jayakumar <arjunjayakumar34@gmail.com>
2026-05-22 15:40:25 +0530

    Configure Celery Redis broker and result backend

--- config/celery.py
+++ config/celery.py
@@ -8,4 +8,20 @@ app = Celery('config')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
+app.conf.broker_url = os.environ.get(
+    "CELERY_BROKER_URL",
+    "redis://localhost:6379/0"
+)
+
+app.conf.result_backend = os.environ.get(
+    "CELERY_RESULT_BACKEND",
+    "redis://localhost:6379/0"
+)
+
+app.conf.accept_content = ["json"]
+app.conf.task_serializer = "json"
+app.conf.result_serializer = "json"
+
+app.conf.timezone = "UTC"
+
 app.autodiscover_tasks()
