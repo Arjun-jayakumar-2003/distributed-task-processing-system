1cb2ee0e39420bc00645dcfc27ee97dd55532988
Arjun Jayakumar <arjunjayakumar34@gmail.com>
2026-05-22 19:02:26 +0530

    Implement database-backed Celery task processing

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,5 +1,20 @@
 from celery import shared_task
+from time import sleep
+
+from .models import Task
+
 
 @shared_task
-def test_task():
-    return "Celery is working"
+def process_task(task_id):
+
+    task = Task.objects.get(id=task_id)
+
+    task.status = "PROCESSING"
+    task.save()
+
+    sleep(5)
+
+    task.result = f"Processed payload: {task.payload}"
+
+    task.status = "SUCCESS"
+    task.save()
